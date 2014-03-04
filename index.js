946dd22d7a0ac775f06a69a8f81f45de651c4cb8
Jean-François Charrière <jean-francois.charriere@bluewin.ch>
2014-03-04 21:59:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+//JFC 20140304
+var handle = {}
+//handle["/startUpload"] = requestHandlers.start;
+handle["/startUpload/start_POST"] = requestHandlers.start_POST;
+handle["/startUpload/start"] = requestHandlers.start;
+handle["/startUpload/upload"] = requestHandlers.upload;
+handle["/startUpload/openGoogleDrive"] = requestHandlers.openGoogleDrive;
+server.start(router.route, handle);
